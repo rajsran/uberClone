a48f73535cab84df0385d3fe52ad2f1681e806d4
rajsran <s3754672@student.rmit.edu.au>
2020-12-07 12:20:49 +1100

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Rajvinder Singh on 7/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

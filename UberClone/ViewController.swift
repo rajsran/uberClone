d4f107d74b09059814eb458297465f6c505b7e63
rajsran <s3754672@student.rmit.edu.au>
2020-12-10 16:03:14 +1100

    login fully working

--- UberClone/ViewController.swift
+++ UberClone/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var driveLabel: UILabel!
     @IBOutlet weak var riderLabel: UILabel!
-    @IBOutlet var passwordField: UIView!
+   
+    @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
@@ -24,8 +25,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+   @IBAction func signUpPressed(_ sender: UIButton) {
+        if emailField.text == "" || passwordField.text == "" {
+        displayAlert(title: "Missing Info", message: "You must enter info")
+            
+        }
+        else {
+            if let email = emailField.text {
+            if let password = passwordField.text {
+                if signUpMode {
+                    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                        if error != nil {
+                            self.displayAlert(title: "error", message: error!.localizedDescription)
+                        } else {
+                            print("success")
+                        }
+                    }
+                }else {
+                    Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                      guard let strongSelf = self else { return }
+                        if error != nil {
+                            self?.displayAlert(title: "error", message: error!.localizedDescription)
+                        } else {
+                            print("success")
+                        }
+                    }
+                }
+              
+                    }
+                }
+            }
+   }
     
-    @IBAction func signUpButtonPressed(_ sender: Any) {
+    func displayAlert(title:String , message:String){
+        let alertController = UIAlertController(title: title, message: message , preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
@@ -35,12 +70,14 @@ class ViewController: UIViewController {
             riderLabel.isHidden = true
             driveLabel.isHidden = true
             riderDriverSwitch.isHidden = true
+            signUpMode = false
         }else {
             signButton.setTitle("signup  ", for: .normal)
             loginButton.setTitle("switch to login ", for: .normal)
             riderLabel.isHidden = false
             driveLabel.isHidden = false
             riderDriverSwitch.isHidden = false
+            signUpMode = true
         }
     }
 }
